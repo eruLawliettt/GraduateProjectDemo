bf323d84743aa55252d72d3ba91e028eb2b35c85
Lawliet <coffeelancerr@gmail.com>
2023-03-06 16:54:30 +0300

    IdentityAdded

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GraduateProjectDemo.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("GraduateProjectDemoIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'GraduateProjectDemoIdentityDbContextConnection' not found.");
+
+builder.Services.AddDbContext<GraduateProjectDemoIdentityDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<GraduateProjectDemoIdentityDbContext>();
 
 builder.Services.AddRazorPages();
 
@@ -14,6 +24,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
