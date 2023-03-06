2d9bc313bb33269a527f4c36b2bdd1dd21532b6c
Lawliet <coffeelancerr@gmail.com>
2023-03-06 19:16:48 +0300

    ChangedAppUIALittle

---
+++ Models/AspNetUserModel.cs
@@ -0,0 +1,7 @@
+namespace GraduateProjectDemo.Models;
+
+public class AspNetUserModel
+{
+    public string UserName { get; set; }
+    public string PhoneNumber { get; set; }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GraduateProjectDemo.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace GraduateProjectDemo.Pages;
 
 public class IndexModel : PageModel
 {
+    public static List<IdentityUser> AllUsers { get; set; }
     private readonly ILogger<IndexModel> _logger;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, UserManager<IdentityUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
     }
 
     public void OnGet()
     {
-
+        AllUsers = _userManager.Users.ToList();
     }
+
 }
